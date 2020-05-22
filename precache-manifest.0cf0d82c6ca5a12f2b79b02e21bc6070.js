51a79d2dcc6688c88d20efce9cf221689c4db079
leejin21c <leejin21c@gmail.com>
2020-05-22 20:15:49 +0900

    Updates

---
+++ precache-manifest.0cf0d82c6ca5a12f2b79b02e21bc6070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffb74b5021e7149fd19c656064d8593",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "521060cb1c11c23c980b",
+    "url": "/movie_app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3b326515f6efe23fe420",
+    "url": "/movie_app/static/js/2.2103d65c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2103d65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521060cb1c11c23c980b",
+    "url": "/movie_app/static/js/main.34d23893.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
